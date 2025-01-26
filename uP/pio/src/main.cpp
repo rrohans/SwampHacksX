ed341526372f62b36e32ccc46e9992281424851b
LocalCephalopod <wdiaz12202@gmail.com>
2025-01-25 22:58:59 -0500

    david bowie

--- uP/pio/src/main.cpp
+++ uP/pio/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <lv_api_map_v8.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while (!Serial)
+    ;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+
+
+  
+  Serial.printf("Serial.println #%d\n", analogRead(GPIO_NUM_4));
+
+
+
+  delay(100);
 }
